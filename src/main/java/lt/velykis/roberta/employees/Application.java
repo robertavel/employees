7aaf7b72cbce9a50b1ab078a54d423db1342a869
Roberta Velykiene <roberta.velykiene@gmail.com>
2019-05-10 21:41:43 +0300

    initial Spring Boot application

---
+++ src/main/java/lt/velykis/roberta/employees/Application.java
@@ -0,0 +1,12 @@
+package lt.velykis.roberta.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
